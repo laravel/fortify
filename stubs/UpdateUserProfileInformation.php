0689f29d4cc3709add3645c03997a89411f532cf
DarthShmev <34663790+DarthShmev@users.noreply.github.com>
2020-09-19 16:15:01 -0400

    StyleCI >:(

--- stubs/UpdateUserProfileInformation.php
+++ stubs/UpdateUserProfileInformation.php
@@ -37,7 +37,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
                 'email_verified_at' => null,
             ])->save();
             $user->sendEmailVerificationNotification();
-            
+
             return;
         }
 
