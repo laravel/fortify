7f50daabdcc6e8a47e003d33cf0c56b12495cf13
Recca Tsai <recca0120@gmail.com>
2020-09-20 23:35:18 +0800

    fix test bug when use sqlite database
    SQLite doesn't support multiple calls to dropColumn / renameColumn in a single modification.

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -32,8 +32,7 @@ class AddTwoFactorColumnsToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('two_factor_secret');
-            $table->dropColumn('two_factor_recovery_codes');
+            $table->dropColumn('two_factor_secret', 'two_factor_recovery_codes');
         });
     }
 }
