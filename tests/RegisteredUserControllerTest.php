55e26932c0f1794b236420422b9e6f863b9c54b6
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-09-28 12:32:23 +0100

    Add tests for overriding the response

--- tests/RegisteredUserControllerTest.php
+++ tests/RegisteredUserControllerTest.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\StatefulGuard;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Fortify\Contracts\RegisterViewResponse;
+use Laravel\Fortify\Http\Responses\RegisterResponse;
 use Mockery;
 
 class RegisteredUserControllerTest extends OrchestraTestCase
@@ -37,5 +38,29 @@ class RegisteredUserControllerTest extends OrchestraTestCase
         $response = $this->post('/register', []);
 
         $response->assertRedirect('/home');
+    }
+
+    public function test_users_can_override_the_default_response()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->mock(RegisterResponse::class)
+                ->shouldReceive('toResponse')
+                ->andReturn(redirect('/custom-route'))
+                ->getMock();
+
+        $this->mock(CreatesNewUsers::class)
+                    ->shouldReceive('create')
+                    ->andReturn(Mockery::mock(Authenticatable::class))
+                    ->shouldReceive('created')
+                    ->andReturn($response);
+
+        $this->mock(StatefulGuard::class)
+                    ->shouldReceive('login')
+                    ->once();
+
+        $response = $this->post('/register', []);
+
+        $response->assertRedirect('/custom-route');
     }
 }
