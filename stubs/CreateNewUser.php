cc0d097dedec3ea60850a34368d3777c34e70673
Dries Vints <dries@vints.io>
2020-10-05 13:32:24 +0200

    Update CreateNewUser.php

--- stubs/CreateNewUser.php
+++ stubs/CreateNewUser.php
@@ -3,9 +3,9 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
-use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
