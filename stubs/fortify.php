673f59c4918b59ea9ba8d0042e3b2aec331c52dd
Tarzis <dominique.leibundgut+github@gmail.com>
2020-11-17 23:36:11 +0100

    [1.x] Fix route prefix (#152)
    * Fix wrong prefix config option
    
    * Fix config documentation typo
    
    Co-authored-by: Dominique Leibundgut <dominique.leibundgut@gmail.com>

--- stubs/fortify.php
+++ stubs/fortify.php
@@ -68,7 +68,7 @@ return [
     | Fortify Routes Prefix / Subdomain
     |--------------------------------------------------------------------------
     |
-    | Here you may specify which prefix Fortify will assign to the all routes
+    | Here you may specify which prefix Fortify will assign to all the routes
     | that it registers with the application. If necessary, you may change
     | subdomain under which all of the Fortify routes will be available.
     |
