87d81e9f9c891100f781a23c1eb74880596d81b2
Sergio Fonseca <sifonsecac@users.noreply.github.com>
2020-12-29 12:11:02 -0600

    Change users table for unique validation
    In case you use another table name or model rather than the users table to store users, the unique validation fails because it searchs in a non existing table. Instead, using its classname in the validation the application will determine correctly the table name to use.

--- stubs/UpdateUserProfileInformation.php
+++ stubs/UpdateUserProfileInformation.php
@@ -26,7 +26,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
                 'string',
                 'email',
                 'max:255',
-                Rule::unique('users')->ignore($user->id),
+                Rule::unique(get_class($user))->ignore($user->id),
             ],
         ])->validateWithBag('updateProfileInformation');
 
