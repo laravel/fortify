5bf292e7091444b9c8152fc9452fc5b461111254
Taylor Otwell <taylorotwell@gmail.com>
2020-12-30 10:25:50 -0600

    import class

--- stubs/FortifyServiceProvider.php
+++ stubs/FortifyServiceProvider.php
@@ -7,6 +7,7 @@ use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
