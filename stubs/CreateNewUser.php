a467f6ca3de47504a32dcd3ceebabd5a48d610b5
Rene-Roscher <43115138+Rene-Roscher@users.noreply.github.com>
2021-02-15 00:21:25 +0100

    The e-mail filter was added
    The e-mail filter was added, otherwise mail addresses can be added in any way (no valid addresses)

--- stubs/CreateNewUser.php
+++ stubs/CreateNewUser.php
@@ -25,7 +25,7 @@ class CreateNewUser implements CreatesNewUsers
             'email' => [
                 'required',
                 'string',
-                'email',
+                'email:filter',
                 'max:255',
                 Rule::unique(User::class),
             ],
