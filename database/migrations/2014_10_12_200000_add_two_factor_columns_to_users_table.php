71b4f732db60f732f521eb39e8043856e012db3a
Pavinthan <13897936+pavinthan@users.noreply.github.com>
2021-03-03 00:23:27 +0530

    Update 2014_10_12_200000_add_two_factor_columns_to_users_table.php

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -14,13 +14,10 @@ class AddTwoFactorColumnsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('two_factor_secret')
-                    ->after('password')
-                    ->nullable();
-
-            $table->text('two_factor_recovery_codes')
-                    ->after('two_factor_secret')
-                    ->nullable();
+            $table->after('password', function ($table) {
+                $table->text('two_factor_secret')->nullable();
+                $table->text('two_factor_recovery_codes')->nullable();
+            });
         });
     }
 
