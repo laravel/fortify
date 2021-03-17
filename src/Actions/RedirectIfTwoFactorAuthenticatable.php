b490728d5e18c73ef341e9c4e8a14b9d39829437
danclaytondev <daniel@cl8on.co.uk>
2021-03-17 13:15:05 +0000

    [2.x] Dispatch Event when Two Factor Challenged (#239)
    * dispatch event when two factor challenged and test
    
    * use statements styleci

--- src/Actions/RedirectIfTwoFactorAuthenticatable.php
+++ src/Actions/RedirectIfTwoFactorAuthenticatable.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\Events\Failed;
 use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Laravel\Fortify\Events\TwoFactorAuthenticationChallenged;
 use Laravel\Fortify\Fortify;
 use Laravel\Fortify\LoginRateLimiter;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -132,6 +133,8 @@ class RedirectIfTwoFactorAuthenticatable
             'login.id' => $user->getKey(),
             'login.remember' => $request->filled('remember'),
         ]);
+
+        TwoFactorAuthenticationChallenged::dispatch($user);
 
         return $request->wantsJson()
                     ? response()->json(['two_factor' => true])
