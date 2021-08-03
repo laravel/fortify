d85165b091b357754a69d481311102ca02993307
Taylor Otwell <taylorotwell@gmail.com>
2021-08-02 19:45:09 -0500

    formatting

--- src/Http/Controllers/TwoFactorAuthenticatedSessionController.php
+++ src/Http/Controllers/TwoFactorAuthenticatedSessionController.php
@@ -8,7 +8,7 @@ use Illuminate\Routing\Controller;
 use Laravel\Fortify\Contracts\FailedTwoFactorLoginResponse;
 use Laravel\Fortify\Contracts\TwoFactorChallengeViewResponse;
 use Laravel\Fortify\Contracts\TwoFactorLoginResponse;
-use Laravel\Fortify\Events\ReplacedRecoveryCode;
+use Laravel\Fortify\Events\RecoveryCodeReplaced;
 use Laravel\Fortify\Http\Requests\TwoFactorLoginRequest;
 
 class TwoFactorAuthenticatedSessionController extends Controller
@@ -59,7 +59,7 @@ class TwoFactorAuthenticatedSessionController extends Controller
         if ($code = $request->validRecoveryCode()) {
             $user->replaceRecoveryCode($code);
 
-            event(new ReplacedRecoveryCode($user, $code));
+            event(new RecoveryCodeReplaced($user, $code));
         } elseif (! $request->hasValidCode()) {
             return app(FailedTwoFactorLoginResponse::class);
         }
