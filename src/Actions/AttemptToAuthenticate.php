7afaf4837d96f1d99a5b9a3978fe47b1880aaeee
Grant <grantholle@users.noreply.github.com>
2021-11-20 17:42:04 +0800

    Use `boolean` rather than `filled` for remember
    Right now, it's only checking if `remember` is filled. For AJAX-based requests (such as with Inertia), passing `remember: false` still remembers their auth session.
    
    If using `boolean` on a non-existent property also is false.

--- src/Actions/AttemptToAuthenticate.php
+++ src/Actions/AttemptToAuthenticate.php
@@ -52,7 +52,7 @@ class AttemptToAuthenticate
 
         if ($this->guard->attempt(
             $request->only(Fortify::username(), 'password'),
-            $request->filled('remember'))
+            $request->boolean('remember'))
         ) {
             return $next($request);
         }
@@ -77,7 +77,7 @@ class AttemptToAuthenticate
             return $this->throwFailedAuthenticationException($request);
         }
 
-        $this->guard->login($user, $request->filled('remember'));
+        $this->guard->login($user, $request->boolean('remember'));
 
         return $next($request);
     }
