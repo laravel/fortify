c40843b2a70cbc84ab24cb756961ae2744ab73f8
Dries Vints <dries@vints.io>
2021-12-06 15:15:25 +0100

    Fix an issue with array to string conversion (#333)

--- stubs/FortifyServiceProvider.php
+++ stubs/FortifyServiceProvider.php
@@ -37,7 +37,9 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
         RateLimiter::for('login', function (Request $request) {
-            return Limit::perMinute(5)->by($request->email.$request->ip());
+            $email = (string) $request->email;
+
+            return Limit::perMinute(5)->by($email.$request->ip());
         });
 
         RateLimiter::for('two-factor', function (Request $request) {
