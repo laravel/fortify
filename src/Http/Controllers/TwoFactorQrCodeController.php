f1a501dc6bba357df37a60cb4579079bddbee24a
Taylor Otwell <taylor@laravel.com>
2022-02-18 10:21:16 -0600

    Update TwoFactorQrCodeController.php

--- src/Http/Controllers/TwoFactorQrCodeController.php
+++ src/Http/Controllers/TwoFactorQrCodeController.php
@@ -19,6 +19,9 @@ class TwoFactorQrCodeController extends Controller
             return [];
         }
 
-        return response()->json(['svg' => $request->user()->twoFactorQrCodeSvg(), 'url' => $request->user()->twoFactorQrCodeUrl()]);
+        return response()->json([
+            'svg' => $request->user()->twoFactorQrCodeSvg(), 
+            'url' => $request->user()->twoFactorQrCodeUrl(),
+        ]);
     }
 }
