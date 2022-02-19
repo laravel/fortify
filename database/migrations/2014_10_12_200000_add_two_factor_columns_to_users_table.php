0ef9054661c23683286a5b41fab0d4bcb9dcdcdd
Julius Kiekbusch <jubeki99@gmail.com>
2022-02-19 16:52:59 +0100

    Fix nullable column

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
             if (Fortify::confirmsTwoFactorAuthentication()) {
                 $table->timestamp('two_factor_confirmed_at')
                         ->after('two_factor_recovery_codes')
-                        ->default(false);
+                        ->nullable();
             }
         });
     }
