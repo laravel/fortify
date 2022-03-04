35c44f36f2a93fb7d3d8b38d6b7794913c69d414
Nicklas Kevin Frank <Frozire@users.noreply.github.com>
2022-03-04 21:27:28 +0100

    [1.x] Ensures route password.confirm is defined when not using views (#368)
    * Ensures route password.confirm is defined when not using views
    
    * Removed space messing with styleci
    
    * Simplified solution to ensure named password.confirm route

--- routes/routes.php
+++ routes/routes.php
@@ -109,8 +109,7 @@ Route::group(['middleware' => config('fortify.middleware', ['web'])], function (
     // Password Confirmation...
     if ($enableViews) {
         Route::get('/user/confirm-password', [ConfirmablePasswordController::class, 'show'])
-            ->middleware([config('fortify.auth_middleware', 'auth').':'.config('fortify.guard')])
-            ->name('password.confirm');
+            ->middleware([config('fortify.auth_middleware', 'auth').':'.config('fortify.guard')]);
     }
 
     Route::get('/user/confirmed-password-status', [ConfirmedPasswordStatusController::class, 'show'])
@@ -118,7 +117,8 @@ Route::group(['middleware' => config('fortify.middleware', ['web'])], function (
         ->name('password.confirmation');
 
     Route::post('/user/confirm-password', [ConfirmablePasswordController::class, 'store'])
-        ->middleware([config('fortify.auth_middleware', 'auth').':'.config('fortify.guard')]);
+        ->middleware([config('fortify.auth_middleware', 'auth').':'.config('fortify.guard')])
+        ->name('password.confirm');
 
     // Two Factor Authentication...
     if (Features::enabled(Features::twoFactorAuthentication())) {
