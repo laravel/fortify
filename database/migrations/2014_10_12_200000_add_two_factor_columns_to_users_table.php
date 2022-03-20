842362d50220f31a333c2640f0b182a0c0994205
Mohamed Magdi <91500450+magdicom@users.noreply.github.com>
2022-03-20 04:52:05 +0200

    fix optional feature: Two-Factor confirmation

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -37,11 +37,11 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(array_merge([
+            $table->dropColumn([
                 'two_factor_secret',
                 'two_factor_recovery_codes',
                 'two_factor_confirmed_at',
-            ]));
+            ]);
         });
     }
 };
