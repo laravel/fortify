71dfca3b29cdee1172514bba736bf93be105c255
Mohamed Magdi <91500450+magdicom@users.noreply.github.com>
2022-03-20 04:33:22 +0200

    Update 2014_10_12_200000_add_two_factor_columns_to_users_table.php

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -41,7 +41,7 @@ return new class extends Migration
                 'two_factor_secret',
                 'two_factor_recovery_codes',
                 'two_factor_confirmed_at',
-            ] : []));
+            ]));
         });
     }
 };
