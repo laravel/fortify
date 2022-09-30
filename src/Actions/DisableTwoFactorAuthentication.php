9a68cf2deb37d1796b6e2fd97d3c61f086868914
Taylor Otwell <taylor@laravel.com>
2022-09-30 08:20:14 -0500

    only save if need to

--- src/Actions/DisableTwoFactorAuthentication.php
+++ src/Actions/DisableTwoFactorAuthentication.php
@@ -15,12 +15,16 @@ class DisableTwoFactorAuthentication
      */
     public function __invoke($user)
     {
-        $user->forceFill([
-            'two_factor_secret' => null,
-            'two_factor_recovery_codes' => null,
-        ] + (Fortify::confirmsTwoFactorAuthentication() ? [
-            'two_factor_confirmed_at' => null,
-        ] : []))->save();
+        if (! is_null($user->two_factor_secret) ||
+            ! is_null($user->two_factor_recovery_codes) ||
+            ! is_null($user->two_factor_confirmed_at)) {
+            $user->forceFill([
+                'two_factor_secret' => null,
+                'two_factor_recovery_codes' => null,
+            ] + (Fortify::confirmsTwoFactorAuthentication() ? [
+                'two_factor_confirmed_at' => null,
+            ] : []))->save();
+        }
 
         TwoFactorAuthenticationDisabled::dispatch($user);
     }
