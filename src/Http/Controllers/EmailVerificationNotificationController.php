1edb5224d98c46eca512d949212e70c408c59de7
Swara <swara09@gmail.com>
2022-12-03 19:37:24 +0100

    Allow translate sent message

--- src/Http/Controllers/EmailVerificationNotificationController.php
+++ src/Http/Controllers/EmailVerificationNotificationController.php
@@ -27,6 +27,6 @@ class EmailVerificationNotificationController extends Controller
 
         return $request->wantsJson()
                     ? new JsonResponse('', 202)
-                    : back()->with('status', Fortify::VERIFICATION_LINK_SENT);
+                    : back()->with('status', __('Verification link sent'));
     }
 }
