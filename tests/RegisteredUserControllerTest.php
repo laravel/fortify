656eeac55dd1d16140a7893d4a88b8b27b8a118e
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-03-03 20:36:49 +0100

    Add Test

--- tests/RegisteredUserControllerTest.php
+++ tests/RegisteredUserControllerTest.php
@@ -4,8 +4,10 @@ namespace Laravel\Fortify\Tests;
 
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Http\Request;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Fortify\Contracts\RegisterViewResponse;
+use Laravel\Fortify\Fortify;
 use Mockery;
 
 class RegisteredUserControllerTest extends OrchestraTestCase
@@ -39,6 +41,33 @@ class RegisteredUserControllerTest extends OrchestraTestCase
 
     public function test_users_can_be_created_and_redirected_to_intended_url()
     {
+        $this->mock(CreatesNewUsers::class)
+                    ->shouldReceive('create')
+                    ->andReturn(Mockery::mock(Authenticatable::class));
+
+        $this->mock(StatefulGuard::class)
+                    ->shouldReceive('login')
+                    ->once();
+
+        $response = $this->withSession(['url.intended' => 'http://foo.com/bar'])
+                        ->post('/register', []);
+
+        $response->assertRedirect('http://foo.com/bar');
+    }
+
+    public function test_request_is_sent_through_pipeline_and_users_can_be_created()
+    {
+        Fortify::registerThrough(function (Request $request) {
+            return [
+                new class () {
+                    public function handle($request, $next)
+                    {
+                        return $next($request);
+                    }
+                },
+            ];
+        });
+
         $this->mock(CreatesNewUsers::class)
                     ->shouldReceive('create')
                     ->andReturn(Mockery::mock(Authenticatable::class));
