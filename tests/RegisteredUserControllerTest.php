676145bb4f71a0a7749fe780a164053a06f7a5bc
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-03-04 11:01:46 +0100

    Fix Code Style

--- tests/RegisteredUserControllerTest.php
+++ tests/RegisteredUserControllerTest.php
@@ -59,7 +59,7 @@ class RegisteredUserControllerTest extends OrchestraTestCase
     {
         Fortify::registerThrough(function (Request $request) {
             return [
-                new class ()
+                new class()
                 {
                     public function handle($request, $next)
                     {
