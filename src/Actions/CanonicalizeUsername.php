a437cb14e8793101906ff699cc45c2db7ab03051
Moritz Friedrich <moritz@matchory.com>
2023-09-01 11:02:21 +0200

    Removed legal disclaimer

--- src/Actions/CanonicalizeUsername.php
+++ src/Actions/CanonicalizeUsername.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * This file is part of fortify, a Matchory application.
- *
- * Unauthorized copying of this file, via any medium, is strictly prohibited.
- * Its contents are strictly confidential and proprietary.
- *
- * @copyright 2020–2023 Matchory GmbH · All rights reserved
- * @author    Moritz Friedrich <moritz@matchory.com>
- */
-
-declare(strict_types=1);
-
 namespace Laravel\Fortify\Actions;
 
 use Illuminate\Support\Str;
