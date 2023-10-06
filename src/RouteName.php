548c678b54f61c4d704ace7ba56d1a27b35c156a
moisish <moisis@digitalmarmalade.co.uk>
2023-10-06 10:26:33 +0100

    add RouteName class

---
+++ src/RouteName.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Laravel\Fortify;
+
+class RouteName
+{
+    /**
+     * Get the full route name for the given name if a named prefix is used.
+     *
+     * @param  string  $routeName
+     * @return string
+     */
+    public static function for(string $routeName)
+    {
+        if (!config('fortify.name_prefix')) {
+            return $routeName;
+        }
+        return config('fortify.name_prefix').'.'.$routeName;
+    }
+}
