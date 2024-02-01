935bfcec050ef586fac5190c3375be9db829fe34
Taylor Otwell <taylor@laravel.com>
2024-02-01 12:44:31 -0600

    formatting

--- src/Http/Controllers/TwoFactorAuthenticationController.php
+++ src/Http/Controllers/TwoFactorAuthenticationController.php
@@ -20,7 +20,7 @@ class TwoFactorAuthenticationController extends Controller
      */
     public function store(Request $request, EnableTwoFactorAuthentication $enable)
     {
-        $enable($request->user(), $request->input('force', false));
+        $enable($request->user(), $request->boolean('force', false));
 
         return app(TwoFactorEnabledResponse::class);
     }
