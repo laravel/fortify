66d5f71f13f80713b28e52ae6e00150ac13b3bce
Tom Welch <tomwelch@me.com>
2024-06-28 12:18:20 +0100

    custom recovery code generator

--- src/RecoveryCode.php
+++ src/RecoveryCode.php
@@ -7,12 +7,34 @@ use Illuminate\Support\Str;
 class RecoveryCode
 {
     /**
+     * The callback that should be used to generate a recovery code.
+     *
+     * @var (\Closure(mixed, string): string)|null
+     */
+    public static $generateRecoveryCodeCallback;
+
+    /**
      * Generate a new recovery code.
      *
      * @return string
      */
     public static function generate()
     {
+        if (static::$generateRecoveryCodeCallback) {
+            return call_user_func(static::$generateRecoveryCodeCallback);
+        }
+
         return Str::random(10).'-'.Str::random(10);
+    }
+
+    /**
+     * Set a callback that should be used when generating a recovery code.
+     *
+     * @param  \Closure(mixed, string): string  $callback
+     * @return void
+     */
+    public static function generateRecoveryCodeUsing($callback)
+    {
+        static::$generateRecoveryCodeCallback = $callback;
     }
 }
