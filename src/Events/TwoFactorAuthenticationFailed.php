81533ee47aa309532c3aea6cc6ddffbc599428c2
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2024-07-19 12:19:54 -0400

    extends TwoFactorAuthenticationEvent

--- src/Events/TwoFactorAuthenticationFailed.php
+++ src/Events/TwoFactorAuthenticationFailed.php
@@ -2,27 +2,7 @@
 
 namespace Laravel\Fortify\Events;
 
-use Illuminate\Queue\SerializesModels;
-
-class TwoFactorAuthenticationFailed
+class TwoFactorAuthenticationFailed extends TwoFactorAuthenticationEvent
 {
-    use SerializesModels;
-
-    /**
-     * The authenticated user.
-     *
-     * @var \Illuminate\Contracts\Auth\Authenticatable
-     */
-    public $user;
-
-    /**
-     * Create a new event instance.
-     *
-     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
-     * @return void
-     */
-    public function __construct($user)
-    {
-        $this->user = $user;
-    }
+    //
 }
