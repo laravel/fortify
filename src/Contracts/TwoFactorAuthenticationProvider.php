2f3326bc3aa002f307f036b167602d396cd84956
Matthieu D <matthieu.deroubaix@gmail.com>
2024-09-09 18:04:10 +0700

    Update TwoFactorAuthenticationProvider.php
    Update contract to reflect new security standard in 2FA

--- src/Contracts/TwoFactorAuthenticationProvider.php
+++ src/Contracts/TwoFactorAuthenticationProvider.php
@@ -7,9 +7,10 @@ interface TwoFactorAuthenticationProvider
     /**
      * Generate a new secret key.
      *
+     * @param  int  $secret_length
      * @return string
      */
-    public function generateSecretKey();
+    public function generateSecretKey($secret_length);
 
     /**
      * Get the two factor authentication QR code URL.
