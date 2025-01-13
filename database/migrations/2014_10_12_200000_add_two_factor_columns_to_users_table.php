laravel/fortify

commit a20e8033e7329b05820007c398f06065a38ae188

Changed migration of users table (#586)
* Changed migration of users table

* Update 2014_10_12_200000_add_two_factor_columns_to_users_table.php

Removed unused use statement

database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Laravel\Fortify\Fortify;
 
 return new class extends Migration
 {
@@ -21,11 +20,9 @@ return new class extends Migration
                 ->after('two_factor_secret')
                 ->nullable();
 
-            if (Fortify::confirmsTwoFactorAuthentication()) {
-                $table->timestamp('two_factor_confirmed_at')
-                    ->after('two_factor_recovery_codes')
-                    ->nullable();
-            }
+            $table->timestamp('two_factor_confirmed_at')
+                ->after('two_factor_recovery_codes')
+                ->nullable();
         });
     }
 
@@ -35,12 +32,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(array_merge([
+            $table->dropColumn([
                 'two_factor_secret',
                 'two_factor_recovery_codes',
-            ], Fortify::confirmsTwoFactorAuthentication() ? [
                 'two_factor_confirmed_at',
-            ] : []));
+            ]);
         });
     }
 };

commit 146e4f911ab73faa6eb314b40922defb716e2f4b

Only drop `two_factor_confirmed_at` column if it exists in the table
Avoid regression bug from #586

Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -32,11 +32,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn([
+            $table->dropColumn(array_filter([
                 'two_factor_secret',
                 'two_factor_recovery_codes',
-                'two_factor_confirmed_at',
-            ]);
+                Schema::hasColumn('users', 'two_factor_confirmed_at') ? 'two_factor_confirmed_at' : null,
+            ]));
         });
     }
 };