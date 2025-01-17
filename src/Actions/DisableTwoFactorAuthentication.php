ee35e5b8ea25cc51f8323e27a839283becd44160
Mior Muhammad Zaki <crynobone@gmail.com>
2025-01-17 23:17:57 +0800

    `DisableTwoFactorAuthentication` should always set `two_factor_confirmed_at` to `null` when it has a value (#589)
    fixes #588

--- src/Actions/DisableTwoFactorAuthentication.php
+++ src/Actions/DisableTwoFactorAuthentication.php
@@ -21,7 +21,7 @@ class DisableTwoFactorAuthentication
             $user->forceFill([
                 'two_factor_secret' => null,
                 'two_factor_recovery_codes' => null,
-            ] + (Fortify::confirmsTwoFactorAuthentication() ? [
+            ] + (Fortify::confirmsTwoFactorAuthentication() || ! is_null($user->two_factor_confirmed_at) ? [
                 'two_factor_confirmed_at' => null,
             ] : []))->save();
 
