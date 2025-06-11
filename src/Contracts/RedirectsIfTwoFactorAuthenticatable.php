25cf2932bd9a3fef7e20570e6fdf1e5a3e12dde9
Jun <kyungjunim@gmail.com>
2025-06-12 01:09:17 +1200

    Add RedirectsIfTwoFactorAuthenticatable contract

---
+++ src/Contracts/RedirectsIfTwoFactorAuthenticatable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Laravel\Fortify\Contracts;
+
+/**
+ * @method void handle(\Illuminate\Http\Request $request, callable $next)
+ */
+interface RedirectsIfTwoFactorAuthenticatable
+{
+    //
+}
