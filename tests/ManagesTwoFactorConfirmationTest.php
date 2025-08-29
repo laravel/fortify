3c5ed8b2b16d23cbbc1167a16dd87c3c091fa2ea
Pushpak Chhajed <pushpak1300@gmail.com>
2025-08-29 14:38:39 +0530

    Fix Formatting

--- tests/ManagesTwoFactorConfirmationTest.php
+++ tests/ManagesTwoFactorConfirmationTest.php
@@ -110,6 +110,7 @@ class ManagesTwoFactorConfirmationTest extends OrchestraTestCase
     public static function confirmationBlockersProvider(): array
     {
         $pastTime = time() - 10;
+
         return [
             'no_secret' => [
                 ['two_factor_secret' => null, 'two_factor_confirmed_at' => null],
