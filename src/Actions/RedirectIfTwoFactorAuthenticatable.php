facceb73af1982e8b4d97416080c171a46dd2526
joe <joexswanson@gmail.com>
2025-10-24 18:07:33 -0500

    Simplify two-factor confirmation check

--- src/Actions/RedirectIfTwoFactorAuthenticatable.php
+++ src/Actions/RedirectIfTwoFactorAuthenticatable.php
@@ -50,14 +50,9 @@ class RedirectIfTwoFactorAuthenticatable
     {
         $user = $this->validateCredentials($request);
 
-        if (Fortify::confirmsTwoFactorAuthentication()) {
-            if (optional($user)->two_factor_secret &&
-                ! is_null(optional($user)->two_factor_confirmed_at) &&
-                in_array(TwoFactorAuthenticatable::class, class_uses_recursive($user))) {
-                return $this->twoFactorChallengeResponse($request, $user);
-            } else {
-                return $next($request);
-            }
+        if (Fortify::confirmsTwoFactorAuthentication() &&
+            is_null(optional($user)->two_factor_confirmed_at)) {
+            return $next($request);
         }
 
         if (optional($user)->two_factor_secret &&
