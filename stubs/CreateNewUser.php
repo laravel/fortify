f9818611daf00512a7db632b1da723670537369a
MohammadRezae <mohammaddevrezaei@gmail.com>
2026-02-14 18:02:19 +0330

    Use explicit query() for User creation

--- stubs/CreateNewUser.php
+++ stubs/CreateNewUser.php
@@ -31,7 +31,7 @@ class CreateNewUser implements CreatesNewUsers
             'password' => $this->passwordRules(),
         ])->validate();
 
-        return User::create([
+        return User::query()->create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
