b56acdd2d7b025d11e4444848b6af9fdbc672ad1
MohammadRezae <mohammaddevrezaei@gmail.com>
2026-02-14 18:09:39 +0330

    Add @throws annotation to create() docblock

--- stubs/CreateNewUser.php
+++ stubs/CreateNewUser.php
@@ -16,7 +16,7 @@ class CreateNewUser implements CreatesNewUsers
     /**
      * Validate and create a newly registered user.
      *
-     * @param array<string, string> $input
+     * @param  array<string, string>  $input
      * @throws ValidationException
      */
     public function create(array $input): User
