0cc37ee594fb770fe5b1a4353a2937b13409ae35
MohammadRezaei <135225767+mohammadRezaei1380@users.noreply.github.com>
2026-02-15 02:29:23 +0330

    Add @throws annotation to create() docblock (#634)
    * Add @throws annotation to create() docblock
    
    * Add @throws annotation to create() docblock
    
    * Add @throws annotation to create() docblock

--- stubs/CreateNewUser.php
+++ stubs/CreateNewUser.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
@@ -16,6 +17,8 @@ class CreateNewUser implements CreatesNewUsers
      * Validate and create a newly registered user.
      *
      * @param  array<string, string>  $input
+     *
+     * @throws ValidationException
      */
     public function create(array $input): User
     {
