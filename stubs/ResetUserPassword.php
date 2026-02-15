48d8b18b925ae747fc9a942ecd0beda051b8f388
MohammadRezaei <135225767+mohammadRezaei1380@users.noreply.github.com>
2026-02-15 22:47:04 +0330

    Add @throws annotation to create() docblock (#635)

--- stubs/ResetUserPassword.php
+++ stubs/ResetUserPassword.php
@@ -5,6 +5,7 @@ namespace App\Actions\Fortify;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Contracts\ResetsUserPasswords;
 
 class ResetUserPassword implements ResetsUserPasswords
@@ -15,6 +16,8 @@ class ResetUserPassword implements ResetsUserPasswords
      * Validate and reset the user's forgotten password.
      *
      * @param  array<string, string>  $input
+     *
+     * @throws ValidationException
      */
     public function reset(User $user, array $input): void
     {
