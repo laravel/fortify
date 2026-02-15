ddb65d51dd4db17c163b00731fdc1ec11f7a8752
MohammadRezae <mohammaddevrezaei@gmail.com>
2026-02-15 09:20:24 +0330

    Add @throws annotation to create() docblock

--- stubs/UpdateUserPassword.php
+++ stubs/UpdateUserPassword.php
@@ -5,6 +5,7 @@ namespace App\Actions\Fortify;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Contracts\UpdatesUserPasswords;
 
 class UpdateUserPassword implements UpdatesUserPasswords
@@ -15,6 +16,8 @@ class UpdateUserPassword implements UpdatesUserPasswords
      * Validate and update the user's password.
      *
      * @param  array<string, string>  $input
+     *
+     * @throws ValidationException
      */
     public function update(User $user, array $input): void
     {
