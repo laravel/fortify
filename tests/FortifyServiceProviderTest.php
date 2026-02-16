40515a254a5266998d44e3ca631c3806441249db
Victoria Suzuki <53244381+vrodriguero@users.noreply.github.com>
2026-02-16 14:32:41 +1000

    Use scoped bindings for Octane compatibility

--- tests/FortifyServiceProviderTest.php
+++ tests/FortifyServiceProviderTest.php
@@ -4,7 +4,10 @@ namespace Laravel\Fortify\Tests;
 
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Fortify\Contracts\RedirectsIfTwoFactorAuthenticatable;
 use Laravel\Fortify\Fortify;
+use Orchestra\Testbench\Attributes\DefineEnvironment;
 
 class FortifyServiceProviderTest extends OrchestraTestCase
 {
@@ -36,5 +39,61 @@ class FortifyServiceProviderTest extends OrchestraTestCase
 
         $response->assertOk();
         $response->assertExactJson(['foo' => 'bar']);
+    }
+
+    #[DefineEnvironment('withTwoFactorAuthentication')]
+    public function test_redirect_if_two_factor_authenticatable_is_resolved_fresh_after_flushing_scoped_instances()
+    {
+        $instanceA = $this->app->make(RedirectsIfTwoFactorAuthenticatable::class);
+        $instanceB = $this->app->make(RedirectsIfTwoFactorAuthenticatable::class);
+
+        $this->assertSame($instanceA, $instanceB);
+
+        $this->app->forgetScopedInstances();
+
+        $instanceC = $this->app->make(RedirectsIfTwoFactorAuthenticatable::class);
+
+        $this->assertNotSame($instanceA, $instanceC);
+    }
+
+    #[DefineEnvironment('withTwoFactorAuthentication')]
+    public function test_redirect_if_two_factor_authenticatable_receives_fresh_guard_after_flushing_scoped_instances()
+    {
+        $instance = $this->app->make(RedirectsIfTwoFactorAuthenticatable::class);
+
+        $guardProperty = new \ReflectionProperty($instance, 'guard');
+        $guardBefore = $guardProperty->getValue($instance);
+
+        // Simulate Octane request boundary.
+        Auth::forgetGuards();
+        $this->app->forgetScopedInstances();
+
+        $freshInstance = $this->app->make(RedirectsIfTwoFactorAuthenticatable::class);
+
+        $guardAfter = $guardProperty->getValue($freshInstance);
+
+        $this->assertNotSame($guardBefore, $guardAfter);
+    }
+
+    #[DefineEnvironment('withTwoFactorAuthentication')]
+    public function test_custom_redirect_if_two_factor_authenticatable_is_resolved_fresh_after_flushing_scoped_instances()
+    {
+        Fortify::redirectUserForTwoFactorAuthenticationUsing(TestRedirectIfTwoFactorAuthenticatable::class);
+
+        $instanceA = $this->app->make(RedirectsIfTwoFactorAuthenticatable::class);
+
+        $this->app->forgetScopedInstances();
+
+        $instanceB = $this->app->make(RedirectsIfTwoFactorAuthenticatable::class);
+
+        $this->assertNotSame($instanceA, $instanceB);
+    }
+}
+
+class TestRedirectIfTwoFactorAuthenticatable implements RedirectsIfTwoFactorAuthenticatable
+{
+    public function handle($request, $next)
+    {
+        return $next($request);
     }
 }
