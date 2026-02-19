5f1148264da20c8a02119ce00511589b30541392
MohammadRezaei <135225767+mohammadRezaei1380@users.noreply.github.com>
2026-02-19 18:12:12 +0330

    Update @return type of toResponse() to mixed for accuracy (#645)
    * Clarify required guard for SPA authentication setup
    
    * add @throws in function UpdateUserProfileInformation.php
    
    * Update @return type of toResponse() to mixed for accuracy

--- src/Http/Responses/SimpleViewResponse.php
+++ src/Http/Responses/SimpleViewResponse.php
@@ -42,7 +42,7 @@ class SimpleViewResponse implements
      * Create an HTTP response that represents the object.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return mixed
      */
     public function toResponse($request)
     {
