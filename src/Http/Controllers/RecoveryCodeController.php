4dcf1effe82603794e3ada8eaa52bc4156b17e12
Taylor Otwell <taylor@laravel.com>
2026-02-22 08:14:52 -0600

    Update return type in RecoveryCodeController

--- src/Http/Controllers/RecoveryCodeController.php
+++ src/Http/Controllers/RecoveryCodeController.php
@@ -14,7 +14,7 @@ class RecoveryCodeController extends Controller
      * Get the two factor authentication recovery codes for authenticated user.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\JsonResponse|array
      */
     public function index(Request $request)
     {
