b4c7efafd1342db2b10d0bec609f8f5b2c65227d
nuno maduro <enunomaduro@gmail.com>
2026-03-10 16:43:19 +0000

    Makes imports consistent

--- stubs/PasswordValidationRules.php
+++ stubs/PasswordValidationRules.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Fortify;
 
+use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Validation\Rules\Password;
 
 trait PasswordValidationRules
@@ -9,7 +10,7 @@ trait PasswordValidationRules
     /**
      * Get the validation rules used to validate passwords.
      *
-     * @return array<int, \Illuminate\Contracts\Validation\Rule|array<mixed>|string>
+     * @return array<int, Rule|array<mixed>|string>
      */
     protected function passwordRules(): array
     {
