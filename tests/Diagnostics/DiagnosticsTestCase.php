9f3f5524a74e4aa0b7042bce9861fcaf08d06326
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2026-08-03 12:09:19 -0700

    Remove support for laravel 11

--- tests/Diagnostics/DiagnosticsTestCase.php
+++ tests/Diagnostics/DiagnosticsTestCase.php
@@ -7,6 +7,15 @@ use Laravel\Fortify\Tests\OrchestraTestCase;
 
 abstract class DiagnosticsTestCase extends OrchestraTestCase
 {
+    protected function setUp(): void
+    {
+        if (! class_exists(DoctorServiceProvider::class)) {
+            $this->markTestSkipped('laravel/doctor is not installed.');
+        }
+
+        parent::setUp();
+    }
+
     protected function getPackageProviders($app)
     {
         return array_merge(parent::getPackageProviders($app), [
